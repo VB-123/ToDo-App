af1817ca5020169075b3e9695e28eb7be9f4119e
Vasanthi B N S <127195382+VB-123@users.noreply.github.com>
2024-01-25 21:49:08 +0530

    Update API.ts

--- my-app/src/API.ts
+++ my-app/src/API.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosResponse } from 'axios'
 
-const baseUrl: string = 'http://localhost:5500/api'
+const baseUrl: string = 'http://localhost:5500'
 
 export const getTodos = async (): Promise<AxiosResponse<ApiDataType>> => {
   try {
